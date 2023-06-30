d6a2574eadcaab621520c6a6808f8c791dc86b15
Kon Rybnikov <k-bx@k-bx.com>
2023-06-30 15:27:02 +0300

    code causing trouble

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,20 @@ async fn main() {
 }
 
 async fn worker() {
+    let context = make_context();
+    let boa_context = tokio::sync::RwLock::new(context);
+    some_worker_func(&boa_context).await;
+}
+
+pub fn make_context<'context>() -> Context<'context> {
     let mut context = Context::default();
     let src2 = Source::from_bytes(r#"var two = 2;"#);
     let _ = context.eval(src2).unwrap();
+    context
+}
+
+pub async fn some_worker_func(
+    boa_context: &tokio::sync::RwLock<boa_engine::Context<'_>>,
+) -> () {
+    println!("do nothing");
 }
